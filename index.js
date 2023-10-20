e76c214e4b5ff199a96ebb9621cfe258e6cad6fe
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-10-20 16:51:03 +0600

    server running

--- index.js
+++ index.js
@@ -26,13 +26,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const productCollection = client.db('prodBD').collection('pro');
+
+    app.post('/pro', async(req,res)=>{
+        const newProducts = req.body;
+        console.log(newProducts);
+        const result = await productCollection.insertOne(newProducts);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
